18cc50a4430bf80f75ccd1ba504064f2bd7610bc
saitama <pakhuridze.tornike@gmail.com>
2024-09-14 18:50:21 +0400

    update project, removed global posts_arr array and save data without it

--- posts_parser/parser.py
+++ posts_parser/parser.py
@@ -5,37 +5,39 @@ import time
 
 start_time = time.time()
 
-posts_arr = []
-
-
-def fetch_and_save(post_id, lock_thread):
+def fetch_and_save(post_id, lock_thread, file):
     url = f"https://jsonplaceholder.typicode.com/posts/{post_id}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         with lock_thread:
-            posts_arr.append(data)
+            # Lock for file operations to prevent race conditions
+            file.write(json.dumps(data, indent=3))
+            file.write(',\n')  # Ensure each JSON object is on a new line
     else:
-        print("Can't fetch data")
+        print(f"Can't fetch data for post ID {post_id}")
 
+def main():
+    lock = threading.Lock()
 
-threads = []
-lock = threading.Lock()
+    with open("posts.json", "w") as file:
+        file.write('[')  # Start the JSON array
 
-for i in range(1, 78):
-    thread = threading.Thread(target=fetch_and_save, args=(i, lock))
-    thread.start()
-    threads.append(thread)
+        threads = []
+        for i in range(1, 78):
+            thread = threading.Thread(target=fetch_and_save, args=(i, lock, file))
+            thread.start()
+            threads.append(thread)
 
-for thread in threads:
-    thread.join()
+        for thread in threads:
+            thread.join()
 
-# sort data after fetching
-posts_arr.sort(key=lambda x: x["id"])
+        # Go back to remove the last comma
+        file.seek(file.tell() - 2, 0)  # Move back to overwrite the last comma
+        file.write(']')  # End the JSON array
 
-# add sorted json
-with open("posts.json", "w") as f:
-    json.dump(posts_arr, f, indent=3)
+if __name__ == "__main__":
+    main()
 
 end_time = time.time()
 
